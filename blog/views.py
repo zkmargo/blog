7dbdb9a7b0c1487f9f9455c00a9032f9ce53c467
zkmargo <zkmargo@gmail.com>
2022-12-21 11:25:40 -0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blog/post_detail.html', context)
